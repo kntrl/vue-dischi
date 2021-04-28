5d831c0dcd0a7d8734131c716fb570c9286b0c22
Lorenzo <lorypsy@gmail.com>
2021-04-28 16:44:16 +0200

    Added genre filter.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
 var app = new Vue({
 	el: "#app",
 	data: {
+		filteredGenre: "all",
 		albums: [],
+		genres: "",
 	},
-	method: {},
+	methods: {
+		//FUNCTION getFilter(objArray,propAsCategory)
+		// objArray -> an array of objects
+		// propAsCategory -> which obj prop to be considered a category
+		// returns : a string array where each string is a UNIQUE category
+		getFilter: function getFilter(objArray, propAsCategory) {
+			const categoriesArray = [];
+			objArray.forEach((element) => {
+				if (!categoriesArray.includes(element[propAsCategory])) {
+					categoriesArray.push(element[propAsCategory]);
+				}
+			});
+			return categoriesArray;
+		},
+	},
 	mounted() {
 		//requesting API the albums array
 		axios
@@ -12,6 +28,9 @@ var app = new Vue({
 				const result = response.data;
 				this.albums = [...result.response];
 				console.log(this.albums);
+
+				this.genres = this.getFilter(this.albums, "genre");
+				console.log(this.genres);
 			});
 	},
 });
